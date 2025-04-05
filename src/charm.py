dd6007d7028317e5e3638967775d3636b3433154
dushu <shu.du@canonical.com>
2025-04-05 17:38:20 -0400

    fix: add tracing

--- src/charm.py
+++ src/charm.py
@@ -76,6 +76,7 @@ from constants import (
     WORKLOAD_CONTAINER,
 )
 from integrations import CertificatesIntegration, CertificatesTransferIntegration
+from lib.charms.tempo_coordinator_k8s.v0.tracing import TracingEndpointRequirer
 from openfga import OpenFGA
 from state import State, requires_state, requires_state_setter
 
@@ -126,6 +127,14 @@ class OpenFGAOperatorCharm(CharmBase):
             refresh_event=self.on.config_changed,
             relation_name=METRIC_RELATION_NAME,
         )
+
+        # Tracing integration
+        self.tracing = TracingEndpointRequirer(
+            self,
+            protocols=['otlp_grpc'],
+        )
+        self.framework.observe(self.tracing.on.endpoint_changed, self._on_tracing_endpoint_changed)
+        self.framework.observe(self.tracing.on.endpoint_removed, self._on_tracing_endpoint_changed)
 
         # OpenFGA relation
         self.framework.observe(
@@ -271,8 +280,6 @@ class OpenFGAOperatorCharm(CharmBase):
         env_vars["OPENFGA_METRICS_ENABLE_RPC_HISTOGRAMS"] = "true"
         env_vars["OPENFGA_METRICS_ENABLED"] = "true"
         env_vars["OPENFGA_DATASTORE_METRICS_ENABLED"] = "true"
-        env_vars["OPENFGA_TRACE_ENABLED"] = "true"
-        env_vars["OPENFGA_TRACE_SAMPLE_RATIO"] = "0.3"
 
         token = self._get_token()
         if token:
@@ -291,7 +298,10 @@ class OpenFGAOperatorCharm(CharmBase):
             env_vars["OPENFGA_GRPC_TLS_CERT"] = str(SERVER_CERT)
             env_vars["OPENFGA_GRPC_TLS_KEY"] = str(SERVER_KEY)
 
-            env_vars["OPENFGA_TRACE_OTLP_TLS_ENABLED"] = "true"
+        if self.tracing.is_ready():
+            env_vars["OPENFGA_TRACE_ENABLED"] = "true"
+            env_vars["OPENFGA_TRACE_OTLP_ENDPOINT"] = self.tracing.get_endpoint("otlp_grpc")
+            env_vars["OPENFGA_TRACE_SAMPLE_RATIO"] = "0.3"
 
         pebble_layer: LayerDict = {
             "summary": "openfga layer",
@@ -670,6 +680,9 @@ class OpenFGAOperatorCharm(CharmBase):
         self._certs_transfer_integration.transfer_certificates(
             self._certs_integration.cert_data, event.relation.id
         )
+
+    def _on_tracing_endpoint_changed(self, event: HookEvent):
+        self._update_workload(event)
 
     def _get_grpc_url(self) -> str:
         k8s_svc = f"{self.app.name}.{self.model.name}.svc.cluster.local:{OPENFGA_SERVER_GRPC_PORT}"
